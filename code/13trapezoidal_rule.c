1f4cce57718389f230586bde5de55a4f7bef952c
DeepeshKalura <deepeshkalurs@gmail.com>
2023-12-14 12:03:41 +0530

    trapezoid rule is completed

---
+++ code/13trapezoidal_rule.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+float f(float x){
+    return (1 / (1+ x*x));
+}
+
+int main(){
+
+    float a, b;
+    printf("Enter the lower limit:  ");
+    scanf("%f", &a);
+    printf("Enter the upper limit:  ");
+    scanf("%f", &b);
+
+    int n;
+    printf("Enter the sub-interval:     ");
+    scanf("%d", &n);
+
+
+    // finding h;
+    float h = (b-a)/n;
+    float sum=0;
+    // formula for trapezoidal rule
+
+    for(int i = 0; i<=n; i++){
+        if(i==0 || i==n){
+            sum += f(i*h);
+        }else {
+            sum += 2 * f(i*h);
+        }
+    }
+
+    printf("Trapezoid Rule:     %f\n", (float) (sum * h)/2);
+
+    return 0;
+}
