4be57b856fcbef6a25c940641e52aaa5499e916d
DeepeshKalura <deepeshkalurs@gmail.com>
2023-12-14 11:14:15 +0530

    natural intergation simpson_3_8 rule done

---
+++ simpson_3_8rule.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+
+float f(float x){
+	return (1 / (1+x*x));
+}
+
+
+
+
+int main(){
+	float a=0,b=2; // lmits
+
+	printf("Enter the lower limits :	");
+	scanf("%f", &a);
+	printf("Enter the upper limits:		");
+	scanf("%f", &b);
+
+	
+	int n=6; // number of intervals
+	printf("Enter the sub-interval:		");
+	scanf("%d", &n);
+	
+	float h=(b-a)/n;
+	float sum=0;
+
+
+	// formula logic simpson 3/8 
+	for(int i = 0; i<=n; i++){
+		if(i==0 || i==n){
+			sum+=f(i*h);
+		}else if(i%3!=0){
+			sum+=3*f(i*h);
+		} else {
+			sum+=2*f(i*h);
+		}
+	}
+
+
+	printf("Simposon answer is:		%f\n", (float)((sum * 3 * h)/ 8));
+}
